ea61c21c5ba47ce9ef9d07de74507c9dda3eaa3e
kamlesh24299 <130337802+kamlesh24299@users.noreply.github.com>
2023-09-09 11:50:03 +0530

    Add files via upload

---
+++ vendorsetup.sh
@@ -0,0 +1,3 @@
+git clone https://github.com/kamlesh24299/11_vendor_xiaomi_sweet.git vendor/xiaomi/sweet
+
+git clone --depth=1 https://github.com/ArrowOS-Devices/android_kernel_xiaomi_sweet.git -b arrow-11.0 kernel/xiaomi/sweet
