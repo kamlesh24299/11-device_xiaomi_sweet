dbe51039f23ae32ac8983adeef52f7532bc80f5e
kamlesh24299 <kamleshvansjalia02@gmail.com>
2023-10-09 17:01:55 +0000

    update_vendorsetup.sh

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,3 +1,7 @@
 git clone https://github.com/kamlesh24299/11_vendor_xiaomi_sweet.git vendor/xiaomi/sweet
 
-git clone --depth=1 https://github.com/ArrowOS-Devices/android_kernel_xiaomi_sweet.git -b arrow-11.0 kernel/xiaomi/sweet
+git clone --depth=1 https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git -b perf kernel/xiaomi/sweet
+
+git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+
+git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-18.1 hardware/xiaomi
