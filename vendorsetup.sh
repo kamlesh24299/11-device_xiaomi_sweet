81e5641c65811039e159df81ff8f86c17d12ff49
kamlesh24299 <130337802+kamlesh24299@users.noreply.github.com>
2023-10-15 14:14:57 +0530

    add anx cam

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -5,3 +5,5 @@ git clone --depth=1 https://github.com/itsshashanksp/kernel_xiaomi_sm6150.git -b
 git clone --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
 
 git clone https://github.com/LineageOS/android_hardware_xiaomi.git -b lineage-18.1 hardware/xiaomi
+
+git clone https://gitlab.com/dhimanparas20/anx.git -b 11-oss vendor/ANXCamera
